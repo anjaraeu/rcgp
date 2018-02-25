0fc1048648ea171cd702cd4d389e4f07227d23bb
Leon <leonekmi@hotmail.com>
2018-02-25 23:31:11 +0100

    Fix for dropdown 2
    Signed-off-by: Leon <leonekmi@hotmail.com>

--- select.js
+++ select.js
@@ -1,6 +1,10 @@
 var url_string = window.location.href;
 var url = new URL(url_string);
 var language_select = url.searchParams.get("language");
+$('#language').dropdown();
+if (language_select != null) {
+    $('#language').dropdown('set selected', language_select);
+}
 $('#language')
   .dropdown({
       onChange: function (value, text, choice) {
@@ -13,6 +17,3 @@ $('#language')
       }
   })
 ;
-if (language_select != null) {
-    $('#language').dropdown('set selected', language_select);
-}
