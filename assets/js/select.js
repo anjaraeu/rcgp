89374753bce133b5a94814ddb971c6b54b5b5821
skid9000 <skid@tuto-craft.com>
2019-03-08 22:04:35 +0100

    Reorganisation.

---
+++ assets/js/select.js
@@ -0,0 +1,19 @@
+var url_string = window.location.href;
+var url = new URL(url_string);
+var language_select = url.searchParams.get("language");
+$('#language').dropdown();
+if (language_select != null) {
+    $('#language').dropdown('set selected', language_select);
+};
+$('#language')
+  .dropdown('setting', {
+      onChange: function (value, text, choice) {
+          var language = value;
+          if (language == "default") {
+              document.location.href = '/';
+          } else {
+              document.location.href = '/?language=' + language;
+          };
+      }
+  })
+;
