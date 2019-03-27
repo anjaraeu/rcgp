e8c29bd1e9e31b788ce7e4a4142d762d9654e911
Skid <skid@tuto-craft.com>
2019-03-27 21:37:43 +0100

    Setup bordel.

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'RCGP'),
 
     /*
     |--------------------------------------------------------------------------
@@ -225,7 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
     ],
 
 ];
