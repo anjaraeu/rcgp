35e019ab110022f5e7f91297707e7c2cd66be8c5
Skid <skid@tuto-craft.com>
2019-03-27 23:17:22 +0100

    Semantic :D

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'RCGP'),
+    'name' => env('APP_NAME', 'RCGP - Random Cute Girls Programming'),
 
     /*
     |--------------------------------------------------------------------------
