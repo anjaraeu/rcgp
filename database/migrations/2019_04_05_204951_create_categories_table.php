bf5ee4bef4a4b1175ab9534f72e2807aa0914096
Leon <leonekmi@hotmail.com>
2019-05-09 18:50:26 +0200

    nocomment...

--- database/migrations/2019_04_05_204951_create_categories_table.php
+++ database/migrations/2019_04_05_204951_create_categories_table.php
@@ -14,7 +14,7 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->unsigned();
             $table->string('name')->unique();
             $table->string('slug')->unique();
             $table->timestamps();
